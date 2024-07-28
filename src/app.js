8fdaefdb5149a4b07d43d98ebfb7a28cccf0d240
sayan maity <cse21109@cemk.ac.in>
2024-07-28 23:41:23 +0530

    all changes in app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import userRouter from "./routes/userRoutes.js";
+import jobRouter from "./routes/jobRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
 const app=express();
 
 
+app.use(
+    cors({
+        origin:[process.env.FRONTED_URL],
+        method:["GET","POST","DELETE","PUT"],
+        credentials:true,
+    })
+)
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/job",jobRouter);
+app.use("api/v1/application",applicationRouter);
 export default app;

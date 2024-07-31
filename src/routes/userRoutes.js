45de08db766c2f20337cb4cd7fa9eee8647af38c
SAYAN MAITY <156517005+Sayancemk@users.noreply.github.com>
2024-07-31 08:25:41 +0000

    feat: Implement sign-out functionality in userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
     signUp,
     signIn,
+    signOut,
 
 
 } from "../controllers/userController.js"
@@ -9,5 +10,8 @@ const router=express.Router();
 
 router.route("/sign-up").post(signUp);
 router.route("/sign-in").post(signIn);
+router.route("/sign-out").get(signOut);
+
+
 
 export default router;
